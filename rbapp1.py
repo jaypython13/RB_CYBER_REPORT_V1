36f1ba2215b9341dbf05a3bd9dfe7e3b62a7e054
Jayasree Varadarajan <140917755+jaypython13@users.noreply.github.com>
2026-01-19 14:26:29 +0000

    Update rbapp1.py

--- rbapp1.py
+++ rbapp1.py
@@ -54,7 +54,7 @@ if hibp_file and ssl_file:
              with st.spinner("🔐 AI is analysing HIBP and SSL Labs data and generating the Cyber Health Check report..."):
                 time.sleep(5)  # simulate AI processing time
     
-                pdf_bytes = generate_pdf_bytes(
+             pdf_bytes = generate_pdf_bytes(
                     business_name=business_name.strip() or "TBD",
                     email=email.strip() or "TBD",
                     website=website.strip() or "TBD",
@@ -65,9 +65,9 @@ if hibp_file and ssl_file:
                     logo_path=RB_LOGO_PATH,  # <-- always RB_logo.jpg
                 )
             
-                st.success("📄 Your Cyber Health Check Report is ready to download")
+             st.success("📄 Your Cyber Health Check Report is ready to download")
                  
-                st.download_button(
+             st.download_button(
                     "Download Report (PDF)",
                     data=pdf_bytes,
                     file_name=f"Cyber_Health_Check_Report_{(business_name or 'TBD').replace(' ', '_')}.pdf",
